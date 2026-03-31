e1fe140fd82c2801f51a065844fea2e4eea131df
Bea Smith <bsmith152@ewu.edu>
2026-03-31 21:12:46 +0000

    completed in 1 hour

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get the button element by its ID
+const button = document.getElementById('myButton');
+
+// Add a click event listener
+button.addEventListener('click', function() {
+    // Open the desired URL in a new tab
+    window.open('https://www.paramountmovies.com/movies/war-of-the-worlds-2005', '_blank');
+});
+
+const button2 = document.getElementById('myButton2');
+
+// Add a click event listener
+button.addEventListener('click', function() {
+    // Open the desired URL in a new tab
+    window.open('https://www.imdb.com/title/tt0407304/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_war%20of%20the%20worlds', '_blank');
+});
